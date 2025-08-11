a48d3a4c061adf0b88ea7950018f0d94624d93ab
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-08-11 15:49:13 -0400

    add main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+import { tvShows } from "../src/shows/data";
+import { useState } from "react";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState();
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection shows={tvShows} setSelectedShow={setSelectedShow} />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
